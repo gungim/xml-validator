cab6fcc4d51ae3b1014ca246a38ba54d6d0e8d63
gungim <gungim@github.com>
2025-11-27 13:40:03 +0700

    fix: sửa lỗi prisma 6

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
   output: "standalone",
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
