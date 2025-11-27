462b4a141542533e83649ca3e396ce2021392ca4
gungim <gungim@github.com>
2025-11-27 13:24:51 +0700

    fix: sửa lỗi prisma 4

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverExternalPackages: ["@prisma/client", "bcryptjs"],
   output: "standalone",
 };
 
