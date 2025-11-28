aff3b2166f949ac93c8bbf592482cc18f489fc25
gungim <gungim@github.com>
2025-11-28 11:39:54 +0700

    fix: lỗi sai typing

--- src/app/lib/hooks/projects.ts
+++ src/app/lib/hooks/projects.ts
@@ -1,60 +1,60 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 import {
-  getProjects,
   createProject,
   getProject,
+  getProjects,
   updateProject,
-} from "../api/projects";
-import { CreateProjectInput, UpdateProjectInput } from "../types/projects";
+} from '../api/projects'
+import { CreateProjectInput, UpdateProjectInput } from '../types/projects'
 
 export function useProjects(workspace_id: string) {
   return useQuery({
-    queryKey: ["projects", workspace_id],
+    queryKey: ['projects', workspace_id],
     queryFn: () => getProjects(workspace_id),
-  });
+  })
 }
 
 export function useCreateProject() {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: (input: CreateProjectInput) => createProject(input),
     onSuccess: (_, variables) => {
       queryClient.invalidateQueries({
-        queryKey: ["projects", variables.workspaceId],
-      });
+        queryKey: ['projects', variables.workspaceId],
+      })
     },
-  });
+  })
 }
 
 export function useProject(projectId: string) {
   return useQuery({
-    queryKey: ["project", projectId],
+    queryKey: ['project', projectId],
     queryFn: () => getProject(projectId),
-  });
+  })
 }
 
 export function useUpdateProject(workspaceId?: string) {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: ({
       projectId,
       input,
     }: {
-      projectId: string;
-      input: UpdateProjectInput;
+      projectId: string
+      input: UpdateProjectInput
     }) => updateProject(projectId, input),
-    onSuccess: (data) => {
+    onSuccess: data => {
       queryClient.invalidateQueries({
-        queryKey: ["project", data?.id],
-      });
+        queryKey: ['project', data.data.id],
+      })
       if (workspaceId) {
         queryClient.invalidateQueries({
-          queryKey: ["projects", workspaceId],
-        });
+          queryKey: ['projects', workspaceId],
+        })
       }
     },
-  });
+  })
 }
